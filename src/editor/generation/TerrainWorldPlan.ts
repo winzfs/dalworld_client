f919ade915ea2396d37d54c85fccebc791f986b1
winzfs <jazzhjm@gmail.com>
2026-05-23 20:16:18 +0900

    fix(editor): route terrain generation through layered underlay pass

--- src/editor/generation/TerrainWorldPlan.ts
+++ src/editor/generation/TerrainWorldPlan.ts
@@ -1,7 +1,7 @@
 import type { EditorTilePlacement } from '../types';
 import type { BasicTerrainGenerationOptions } from './TerrainGenerator';
-import { generateWorldPlanTerrainDebug } from './TerrainWorldPlanDebug';
+import { generateWorldPlanTerrainLayered } from './TerrainWorldPlanLayered';
 
 export async function generateWorldPlanTerrain(options: BasicTerrainGenerationOptions): Promise<EditorTilePlacement[]> {
-  return generateWorldPlanTerrainDebug(options);
+  return generateWorldPlanTerrainLayered(options);
 }
