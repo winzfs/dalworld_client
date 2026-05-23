b03358d66b6d13c7ecd66560be87aff87edd3fec
winzfs <jazzhjm@gmail.com>
2026-05-23 19:58:19 +0900

    fix(editor): stop forcing transparent black on all rock terrain

--- src/editor/generation/TerrainWorldPlan.ts
+++ src/editor/generation/TerrainWorldPlan.ts
@@ -1,7 +1,7 @@
 import type { EditorTilePlacement } from '../types';
 import type { BasicTerrainGenerationOptions } from './TerrainGenerator';
-import { generateWorldPlanTerrainV3 } from './TerrainWorldPlanV3';
+import { generateWorldPlanTerrainV2 } from './TerrainWorldPlanV2';
 
 export async function generateWorldPlanTerrain(options: BasicTerrainGenerationOptions): Promise<EditorTilePlacement[]> {
-  return generateWorldPlanTerrainV3(options);
+  return generateWorldPlanTerrainV2(options);
 }
