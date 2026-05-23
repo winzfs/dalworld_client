f031c464f548d65d253f17cb12fc46809ca131a4
winzfs <jazzhjm@gmail.com>
2026-05-23 20:03:57 +0900

    feat(editor): route terrain generation through rock debug wrapper

--- src/editor/generation/TerrainWorldPlan.ts
+++ src/editor/generation/TerrainWorldPlan.ts
@@ -1,7 +1,7 @@
 import type { EditorTilePlacement } from '../types';
 import type { BasicTerrainGenerationOptions } from './TerrainGenerator';
-import { generateWorldPlanTerrainV2 } from './TerrainWorldPlanV2';
+import { generateWorldPlanTerrainDebug } from './TerrainWorldPlanDebug';
 
 export async function generateWorldPlanTerrain(options: BasicTerrainGenerationOptions): Promise<EditorTilePlacement[]> {
-  return generateWorldPlanTerrainV2(options);
+  return generateWorldPlanTerrainDebug(options);
 }
