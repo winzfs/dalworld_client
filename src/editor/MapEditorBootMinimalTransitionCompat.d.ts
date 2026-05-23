f6f9b7370b013700e959cac4ec76ba0abe6404f2
winzfs <jazzhjm@gmail.com>
2026-05-23 21:48:10 +0900

    revert(editor): remove broad transition compatibility type

--- src/editor/MapEditorBootMinimalTransitionCompat.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-declare global {
-  interface Object {
-    /** Compatibility for MapEditorBootMinimal.WorldCellTransition until the inline type is safely updated. */
-    targetY: number;
-  }
-}
