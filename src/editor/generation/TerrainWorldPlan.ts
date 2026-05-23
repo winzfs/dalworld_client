49ca3b6ecff24858439af3f26d275237cb2ede33
winzfs <jazzhjm@gmail.com>
2026-05-23 21:01:08 +0900

    feat(editor): use natural terrain visual patch pass

--- src/editor/generation/TerrainWorldPlan.ts
+++ src/editor/generation/TerrainWorldPlan.ts
@@ -1,7 +1,7 @@
 import type { EditorTilePlacement } from '../types';
 import type { BasicTerrainGenerationOptions } from './TerrainGenerator';
-import { generateWorldPlanTerrainLayeredV2 } from './TerrainWorldPlanLayeredV2';
+import { generateWorldPlanTerrainNatural } from './TerrainWorldPlanNatural';
 
 export async function generateWorldPlanTerrain(options: BasicTerrainGenerationOptions): Promise<EditorTilePlacement[]> {
-  return generateWorldPlanTerrainLayeredV2(options);
+  return generateWorldPlanTerrainNatural(options);
 }
